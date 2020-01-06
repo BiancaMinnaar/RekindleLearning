6e787b5cde157eeed1ccc6824e382db380bba5e0
Bianca Minnaar <bminnaar@gmail.com>
2020-01-06 16:08:06 +0200

    Add CourseWideButton control

---
+++ RekindleLearing/Trunk/View/Control/CourseWideButtonControlView.xaml.cs
@@ -0,0 +1,13 @@
+using RekindleLearing.Root;
+using RekindleLearing.Trunk.ViewModel.Control;
+
+namespace RekindleLearing.Trunk.View.Control
+{
+    public partial class CourseWideButtonControlView : ControlBase<CourseWideButtonControlView, CourseWideButtonControlViewModel>
+    {
+        public CourseWideButtonControlView()
+        {
+            InitializeComponent();
+        }
+    }
+}
