2f08ef945c343a19bd6b57154397e0c0163c0127
Bianca Minnaar <bminnaar@gmail.com>
2020-01-07 20:37:08 +0200

    Add CourseListView

---
+++ RekindleLearing/Trunk/View/Control/CourseListControlView.xaml.cs
@@ -0,0 +1,13 @@
+using RekindleLearing.Root;
+using RekindleLearing.Trunk.ViewModel.Control;
+
+namespace RekindleLearing.Trunk.View.Control
+{
+    public partial class CourseListControlView : ControlBase<CourseListControlView, CourseListControlViewModel>
+    {
+        public CourseListControlView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ RekindleLearing/Trunk/ViewModel/Control/CourseListControlViewModel.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using System.ComponentModel;
+using RekindleLearing.Root.ViewModel;
+
+namespace RekindleLearing.Trunk.ViewModel.Control
+{
+    public class CourseListControlViewModel : ProjectBaseViewModel, INotifyPropertyChanged
+    {
+        public new event PropertyChangedEventHandler PropertyChanged;
+
+        private List<CourseViewModel> courses;
+        public List<CourseViewModel> Courses
+        {
+            get { return courses; }
+            set
+            {
+                courses = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Courses"));
+            }
+        }
+
+        //#Bonsai Snippets - Properties
+    }
+}
